311e0f742307e59810a4d5149fc244e922370ca7
Haulund <steffen.haulund@gmail.com>
2024-01-18 12:59:06 +0100

    second assignment

--- src/main/java/com/tcs/reactiveassignment/service/SomeService.java
+++ src/main/java/com/tcs/reactiveassignment/service/SomeService.java
@@ -5,8 +5,18 @@ import reactor.core.publisher.Mono;
 public class SomeService {
     public Mono<String> namesMonoMapFilter(int stringLength){
         return Mono.just("test")
+        .defaultIfEmpty("test")
         .map(String::toUpperCase)
         .filter(e -> e.length() == stringLength)
         .log();
+    }
+    public Mono<String> namesMonoMapFilterSwitchIfEmpty(int stringLength){
+        return Mono.just("test")
+        .switchIfEmpty(namesMonoMapFilter(4))
+        .map(String::toUpperCase)
+        .filter(e -> e.length() == stringLength)
+        .log();
+
+        
     }
 }
