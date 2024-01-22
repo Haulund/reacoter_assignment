b0b93df3f601f0be2dcaa6c5555f5a7637867105
Haulund <steffen.haulund@gmail.com>
2024-01-22 09:53:37 +0100

    assignment three

--- src/main/java/com/tcs/reactiveassignment/service/SomeService.java
+++ src/main/java/com/tcs/reactiveassignment/service/SomeService.java
@@ -9,4 +9,23 @@ public class SomeService {
         .filter(e -> e.length() == stringLength)
         .log();
     }
+    public Mono<String> namesMonoMapFilterSwitchIfEmpty(int stringLength){
+        return Mono.just("test")
+        .switchIfEmpty(namesMonoMapFilter(4))
+        .map(String::toUpperCase)
+        .filter(e -> e.length() == stringLength)
+        .log();
+    }
+
+    public static String throwErrorIfABC(String str) {
+        if (str.equals("abc")) {
+            return new RuntimeException("runtime error").getMessage();
+        } else {
+            return str;
+        }
+    }
+    
+    public Mono<String> exceptionMonoOnErrorContinue(String input) {
+        return Mono.just(input).map(SomeService::throwErrorIfABC).log();
+    }
 }
--- src/test/java/com/tcs/reactiveassignment/NamesMonoMapFilterTest.java
+++ src/test/java/com/tcs/reactiveassignment/NamesMonoMapFilterTest.java
@@ -8,9 +8,10 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class NamesMonoMapFilterTest {
+    SomeService someService = new SomeService();
+
     @Test
     public void testMonoString(){
-        SomeService someService = new SomeService();
         Mono<String> namesMono_map_filter_String = someService.namesMonoMapFilter(4);
         Mono<String> namesMono_map_filter_String_Error = someService.namesMonoMapFilter(5);
         Mono<String> namesMono_map_filter_String_Error_Two = someService.namesMonoMapFilter(3);
@@ -26,5 +27,20 @@ public class NamesMonoMapFilterTest {
 
         StepVerifier.create(namesMono_map_filter_String_Error_Two)
         .expectError();
+
+    }
+    
+    @Test
+    public void testsForAssignmentThree(){
+       /*  StepVerifier.create(someService.exceptionMonoOnErrorContinue("string"))
+        .expectNext("string")
+        .expectComplete()
+        .verify(); */
+
+        StepVerifier.create(someService.exceptionMonoOnErrorContinue("abc"))
+        .expectNext("runtime error")
+        .expectComplete()
+        .verify();
+       
     }
 }
